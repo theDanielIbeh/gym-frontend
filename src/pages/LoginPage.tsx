e566561c78ff45addea970bc4e5276fcb55cf827
William Pepple <williampepple1@gmail.com>
2024-01-16 14:00:49 +0100

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/useAuth';
 import { AuthService } from '../service/AuthService'; 
 
 // Define the type for location state
